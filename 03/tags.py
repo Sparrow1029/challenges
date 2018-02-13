17a483b86935bc8d180e8d308cbee08efa9efe6d
Sparrow1029 <arayart88@gmail.com>
2018-02-13 10:11:54 -0500

    challenge 03 and 10

--- 03/tags.py
+++ 03/tags.py
@@ -1,30 +1,38 @@
 import xml.etree.ElementTree as ET
 from collections import Counter
+from difflib import SequenceMatcher
+import re
+from itertools import product
 
 TOP_NUMBER = 10
 RSS_FEED = 'rss.xml'
 SIMILAR = 0.87
+IDENTICAL = 1.0
+REPLACE_CHARS = str.maketrans('-', ' ')
 
-xmlTree = ET.parse(RSS_FEED)
+root = ET.parse(RSS_FEED).getroot()
 
 def get_tags():
     """Find all tags in RSS_FEED.
     Replace dash with whitespace."""
-    elemList = []
-    for elem in xmlTree.iter():
-        fixed = elem.tag.replace(" ", "-")
-        elemList.append(fixed)
-    return elemList
+    tags = [e.text for e in root.iter('category')]
+    return [tag.translate(REPLACE_CHARS) for tag in tags]
 
 def get_top_tags(tags):
     """Get the TOP_NUMBER of most common tags"""
-    c = Counter(tags)
-    return c.most_common(TOP_NUMBER)
+    return Counter(tags).most_common(TOP_NUMBER)
 
 
 def get_similarities(tags):
     """Find set of tags pairs with similarity ratio of > SIMILAR"""
-
+    tags = set(tags)
+    for pair in product(tags, tags):
+        if pair [0][0] != pair[1][0]:
+            continue
+        pair = tuple(sorted(pair))
+        similarity = SequenceMatcher(None, *pair).ratio()
+        if SIMILAR < similarity < IDENTICAL:
+            yield pair
 
 
 if __name__ == "__main__":
--- 10/hangman.py
+++ 10/hangman.py
@@ -1,5 +1,5 @@
 from string import ascii_lowercase
-import sys
+import sys, os
 
 from movies import get_movie as get_word  # keep interface generic
 from graphics import hang_graphics
@@ -7,13 +7,83 @@ from graphics import hang_graphics
 ASCII = list(ascii_lowercase)
 HANG_GRAPHICS = list(hang_graphics())
 ALLOWED_GUESSES = len(HANG_GRAPHICS)
-PLACEHOLDER = '_'
+# PLACEHOLDER = '_'
 
 
 class Hangman(object):
-    pass 
+    """Main game class"""
 
-# or use functions ...
+    def __init__(self, word):
+        """Storing correct guess values as dict of 0 - 1 switches.
+        self.hidden displays current part of word player has guessed."""
+        self.word = word
+        self.guessed = []
+        self.wrong = 0
+        self.right = {x.lower(): 0 for x in set(filter(lambda x: x.lower() in
+                                                    ascii_lowercase, word))}
+        self.hidden = [c if c.lower() not in ascii_lowercase else '_' for c in word]
+
+    def guess(self):
+        """Handles player input and returns their guess."""
+        while True:
+            letter = input('Guess: ')
+            try:
+                int(letter)
+                print("Letters only please.")
+                continue
+            except ValueError:
+                pass
+            if len(letter) > 1:
+                print('One at a time, hoss.')
+                continue
+            elif not len(letter):
+                print('Please enter a guess.')
+                continue
+            elif letter.lower() not in ascii_lowercase:
+                print("Letters only please.")
+                continue
+            elif letter.lower() in self.guessed:
+                print("Already guessed that one.")
+                continue
+            break
+        self.guessed.append(letter.lower())
+        return letter.lower()
+
+    def display(self):
+        """Function to handle displaying current game state."""
+        print(HANG_GRAPHICS[self.wrong])
+        print('\nPrevious guesses: {}\n'.format(
+            ' '.join([s.upper() for s in self.guessed])))
+        print(''.join(self.hidden), end="\n\n")
+
+    def update_hidden(self, letter):
+        """Helper function to update what part of word is displayed."""
+        word = self.word
+        letter = letter.lower()
+        for i in range(len(word)):
+            if word[i].lower() == letter:
+                self.hidden[i] = word[i]
+
+    def chk(self, letter):
+        """Check correctness of guess."""
+        c = letter.lower()
+        if c in self.right.keys():
+            self.right[c] = 1
+            self.update_hidden(letter)
+        else:
+            self.wrong += 1
+
+    def win(self):
+        """Test for win state."""
+        if all(x == 1 for x in self.right.values()):
+            return True
+        return False
+
+    def lose(self):
+        """Test for losing state."""
+        if self.wrong >= ALLOWED_GUESSES-1:
+            return True
+        return False
 
 
 if __name__ == '__main__':
@@ -21,6 +91,24 @@ if __name__ == '__main__':
         word = sys.argv[1]
     else:
         word = get_word()
-    print(word)
 
-    # init / call program
+    game = Hangman(word)  # Instantiate game class
+
+    # init main game loop
+    while True:
+        os.system('clear')
+        # print(word)
+        # print(game.right)
+        game.display()
+        letter = game.guess()
+        game.chk(letter)
+        if game.win():
+            os.system('clear')
+            game.display()
+            sys.exit("YOU WIN! Your neck is saved.")
+        elif game.lose():
+            os.system('clear')
+            game.display()
+            sys.exit("ACK! You Died. Better luck next life, pardner.")
+        else:
+            continue
